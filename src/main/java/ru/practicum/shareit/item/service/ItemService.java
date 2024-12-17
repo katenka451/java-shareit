a56527e4dcbbfa62cabdcadd3870af630ec80037
katenka451 <pozor451@yandex.ru>
2024-12-17 16:58:56 +0300

    (add-controllers) checkstyle fix

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -7,9 +7,13 @@ import java.util.List;
 public interface ItemService {
 
     List<ItemDto> getAllUserItems(Long userId);
+
     ItemDto getItemById(Long itemId);
+
     List<ItemDto> searchItems(String text, Long userId);
+
     ItemDto createItem(ItemDto newItem, Long userId);
+
     ItemDto updateItem(Long itemId, ItemDto updItem, Long userId);
 
 }
